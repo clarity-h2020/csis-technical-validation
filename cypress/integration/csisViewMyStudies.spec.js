e01b799a9ccfd16652da37b52a65ba1ccf1a81ea
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-01-31 16:05:30 +0100

    #3 basic local profile test

---
+++ cypress/integration/csisViewMyStudies.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="Cypress" />
+// import Cypress from 'cypress';
+// import cy from 'cypress';
+let cyEnv = Cypress.env();
+
+// ugly workaround for https://github.com/clarity-h2020/csis-technical-validation/issues/5
+Cypress.config('baseUrl', cyEnv.baseUrl);
+
+describe('CSIS view my studies', function() {
+	before(() => {
+		cy.clearCookies();
+		cy.getCookies().should('be.empty');
+		
+		cy.visit('user/login');
+		cy.get('#edit-name').type(Cypress.env('username'));
+		cy.get('#edit-pass').type(Cypress.env('password'));
+		cy.get('#edit-submit').click();
+		cy.get('.field--name-username > .field__item').contains(Cypress.env('username'));
+
+		cy.getCookies().should('not.be.empty');
+		cy.getCookies().should('have.length', 1);
+	});
+
+	it('view my studies', function() {
+		cy.visit('/user/');
+		cy.get('.tabs > :nth-child(2) > a').click();
+		cy.location().should((loc) => {
+			expect(loc.pathname).to.contain('/studies') ;
+		});
+
+		
+	});
+
+	/**
+	 * runs once after all tests in the block
+	 */
+	after(() => {
+		cy.visit('/user/logout');
+		cy.get('#block-clarity-useraccountmenu > .menu > .menu-item > a').contains('Login');
+		cy.clearCookies();
+		cy.getCookies().should('be.empty');
+	});
+});
