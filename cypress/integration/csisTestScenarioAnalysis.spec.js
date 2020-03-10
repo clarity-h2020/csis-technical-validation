f2581a174f28991a8fdcebe5141b197613499aa5
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-03-10 11:22:33 +0100

    #15 Scenario Analysis Tests disabled

---
+++ cypress/integration/csisTestScenarioAnalysis.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+// import Cypress from 'cypress';
+// import cy from 'cypress';
+let cyEnv = Cypress.env();
+
+// ugly workaround for https://github.com/clarity-h2020/csis-technical-validation/issues/5
+Cypress.config('baseUrl', cyEnv.baseUrl);
+
+describe('CSIS test Scenario Analysis Component', function () {
+    before(() => {
+        // this does not work. See 
+        cy.loginToCSIS();
+    });
+
+    // wontfix: https://github.com/clarity-h2020/csis-technical-validation/issues/15
+    it.skip('View Risk and Impact Map', function () {
+        cy.visit('/study/35/step/1528/view/scenario-analysis');
+    });
+
+	/**
+	 * runs once after all tests in the block
+	 */
+    after(() => {
+        cy.logoutFromCSIS();
+    });
+
+});
