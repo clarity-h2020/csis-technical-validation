edd2dea36a380e4a9446c6947aa6feb4ad00e6ae
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-03-23 13:04:13 +0100

    #3 EMIKAT tests

--- cypress/integration/csisCheckEmikatStatusPage.spec.js
+++ cypress/integration/csisCheckEmikatStatusPage.spec.js
@@ -11,9 +11,30 @@ describe('EMIKAT Status', function () {
         cy.loginToCSIS();
     });
 
-    it('Check EMIKAT Status Page', function () {
+    it('Check last EMIKAT calculations test', function () {
         cy.visit('/maintenance/check-emikat-results');
+        cy.get('.page-title').contains('Results for Emikat test Study');
+        cy.get('.total-batchjob-count').should('have.text', '13');
+        cy.get('.batchjobs').find('li').should('have.length', 13);
+        cy.get('.total-warning-count').should('have.text', '0');
+
+        // It seems that there is no built-in convenient method to skip all remaining tests   
+        // if one test fails. See https://github.com/cypress-io/cypress/issues/518 :-(
+
+        cy.visit('/maintenance/trigger-emikat-test');
+        cy.get('.trigger-failure').should('not.exist');
+        cy.get('.trigger-success').contains('Updates in Test Study were successfully sent to Emikat. ');
+    
     });
+
+    /**
+     * Disabled. Is triggered even if previous test fails.
+     */
+    /*it('trigger EMIKAT calculations test', function () {
+        cy.visit('/maintenance/trigger-emikat-test');
+        cy.get('.trigger-failure').should('not.exist');
+        cy.get('.trigger-success').contains('Updates in Test Study were successfully sent to Emikat. ');
+    });*/
 
 	/**
 	 * runs once after all tests in the block
