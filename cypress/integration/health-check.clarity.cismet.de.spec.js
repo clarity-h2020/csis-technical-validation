87f574b3da54bd2177345d85bbefbe497d8402b1
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-06-10 12:59:44 +0200

    #48 update test specs

--- cypress/integration/health-check.clarity.cismet.de.spec.js
+++ cypress/integration/health-check.clarity.cismet.de.spec.js
@@ -5,7 +5,7 @@
 /**
  * context() is identical to describe() and specify() is identical to it(), so choose whatever terminology works best for you.
  */
-describe('poor mans service monitoring', function() {
+describe('poor mans service monitoring', function () {
 	/**
 	 * runs once before all tests in the block
 	 */
@@ -22,16 +22,9 @@ describe('poor mans service monitoring', function() {
 	 * See https://github.com/clarity-h2020/csis-technical-validation/issues/2
 	 * for why this is necessary
 	 */
-	it('cheack service status', function() {
+	it('check service status', function () {
 		cy.visit('/');
-		for(let i = 6; i <= 32;i++){
-			cy.get(`[data-id="${i}"] > .bg-success`).should('exist');
-			cy.get(`[data-id="${i}"] > .bg-danger`).should('not.exist');
-		}
-
-		// DRUPAL CAS, only one element
-		cy.get(':nth-child(6) > .list-group > .service_li > .bg-success').should('exist');
-		cy.get(':nth-child(6) > .list-group > .service_li > .bg-danger').should('not.exist');
+		cy.get('.bg-danger > .badge').should('not.exist');
 	});
 
 	/**
