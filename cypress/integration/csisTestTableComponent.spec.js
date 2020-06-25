5946afa3ccdecc206f22c56a405a0cbe64c560c2
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-06-25 09:13:56 +0200

    bugfix

--- cypress/integration/csisTestTableComponent.spec.js
+++ cypress/integration/csisTestTableComponent.spec.js
@@ -16,7 +16,8 @@ describe('CSIS test Table Component', function () {
         cy.visit('/apps/simple-table-component/build/HazardLocalEffectsTable/?host=https://csis.myclimateservice.eu&study_uuid=ae028e6a-ba85-4358-9c4a-4f29faa2ca92&study_area=POLYGON((23.706361%2037.918698,23.706361%2037.957689,23.780862%2037.957689,23.780862%2037.918698,23.706361%2037.918698))&emikat_id=3189&datapackage_uuid=2434ce93-93d4-4ca2-8618-a2de768d3f16&write_permissions=1&study_variant=BASELINE&time_period=20410101-20701231&emissions_scenario=rcp45&event_frequency=Rare');
         cy.get('.-next > .-btn').click();
         cy.get('.select-wrap > select').select('100');
-        cy.get(':nth-child(2) > button').click();
+        // FIXME: give download button an id!
+        cy.get('main > :nth-child(3) > button').click();
     });
 
 	/**
