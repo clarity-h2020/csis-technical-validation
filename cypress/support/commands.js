98cb41c6be6782545a7469b65c720c5541b19ac5
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-07-29 13:57:35 +0200

    #109 renamed log-in to login in csis-dev

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -81,7 +81,7 @@ Cypress.Commands.add('logoutFromCSIS', () => {
 
     cy.visit('/user/logout');
     // 'Log in' in PROD, 'Login' in DEV. WTF?
-    cy.get('#block-clarity-useraccountmenu > .menu > .menu-item > a').contains('Log in');
+    cy.get('#block-clarity-useraccountmenu > .menu > .menu-item > a').contains('Login');
     // This does not work: See https://github.com/clarity-h2020/csis-technical-validation/issues/6
     cy.clearCookies();
     // Skip the check. :-( 
